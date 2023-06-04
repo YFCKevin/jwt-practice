67365b509acacabad8d4e4617eafbaf468b837cd
yifan <yifanchen0914@gmail.com>
2023-06-04 20:47:47 +0800

    [feat]: add isTokenValid and isTokenExpired function and check it on Jwt filter class

--- src/main/java/yfckevin/jwtpractice/security/JWTAuthenticationFilter.java
+++ src/main/java/yfckevin/jwtpractice/security/JWTAuthenticationFilter.java
@@ -4,13 +4,17 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import yfckevin.jwtpractice.service.AuthService;
 import yfckevin.jwtpractice.util.JWTUtil;
 
 import java.io.IOException;
@@ -19,24 +23,47 @@ import java.util.Optional;
 
 // 繼承OncePerRequestFilter，代表每個request都會經過此filter
 @Component
+@RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter{
 
-    @Autowired
+
     private JWTUtil jwtUtil;
+    private AuthService authService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Optional
-                .ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
-                .filter(v -> v.startsWith("bearer "))
-                .ifPresent(v -> {
-                    final String userId = this.jwtUtil.verify(v.substring(7));
-                    final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                            new UsernamePasswordAuthenticationToken(userId, null, null);
-                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                });
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
 
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtUtil.verify(jwt);
+
+        // 如果有傳入email且尚未經過驗證(Authentication)
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            // 帶入email去資料庫取出userDetails
+            UserDetails userDetails = this.authService.loadUserByUsername(userEmail);
+            // 再來檢核userDetails中的userEmail是否相同，且token是否過期
+            if(jwtUtil.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/yfckevin/jwtpractice/util/JWTUtil.java
+++ src/main/java/yfckevin/jwtpractice/util/JWTUtil.java
@@ -3,6 +3,7 @@ package yfckevin.jwtpractice.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -15,7 +16,7 @@ public class JWTUtil {
     private int lifeTime;
 
     // 產生token，將userId簽署到token內，供之後的CRUD用途
-    public String sign(String userId){
+    public String sign(String userEmail){
         return Optional
                 .of(new Date())
                 .map(v -> Jwts
@@ -24,21 +25,41 @@ public class JWTUtil {
                         .setAudience(null)
                         .setExpiration(new Date(System.currentTimeMillis() + lifeTime))
                         .setNotBefore(v)
+                        // JWT發行時間，代表new Date()
                         .setIssuedAt(v)
-                        .setId(userId)
+                        .setId(userEmail)
                         .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
                         .compact()
                 )
                 .get();
     }
 
-    // 驗證token，是否帶有合法的userId
-    public String verify(String token){
-        return Jwts
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userEmail = verify(token);
+        return (userEmail.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expirationDate = Jwts
                 .parserBuilder()
                 .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
                 .build()
                 .parseClaimsJwt(token)
+                .getBody()
+                .getExpiration();
+
+        return expirationDate.before(new Date());
+    }
+
+    // 驗證token，是否帶有合法的userEmail，並取出此userEmail
+    public String verify(String token){
+        return Jwts
+                .parserBuilder()
+                // signing key主要用來驗證
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJwt(token)
+                // 取出all claims
                 .getBody()
                 .getId();
     }
